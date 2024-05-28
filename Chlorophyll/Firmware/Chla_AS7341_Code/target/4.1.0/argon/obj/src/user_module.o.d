f11d884359356561899963ff20c4410a593eb60f
jessiewynne <106984291+jessiewynne@users.noreply.github.com>
2024-05-28 08:53:25 -0400

    Delete Chlorophyll/Firmware/Chla_AS7341_Code directory

--- Chlorophyll/Firmware/Chla_AS7341_Code/target/4.1.0/argon/obj/src/user_module.o.d
+++
@@ -1,6 +0,0 @@
-/Users/jessiewynne/Chl_SD_RTC_AS7341/target/4.1.0/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
