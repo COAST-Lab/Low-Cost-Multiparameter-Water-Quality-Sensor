f6f67ef632ea8d948f4c132a14c540d4c0cff4f6
jessiewynne <106984291+jessiewynne@users.noreply.github.com>
2024-06-13 10:39:46 -0400

    Update Turbidity_Measurements_Analysis.py

--- Turbidity/Data Analysis Scripts/Turbidity_Measurements_Analysis.py
+++ Turbidity/Data Analysis Scripts/Turbidity_Measurements_Analysis.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 # Read the CSV file
-df = pd.read_csv('/Users/jessiewynne/turb_dock_test_4_15_24_no_DI.csv')
+df = pd.read_csv('/Users/jessiewynne/chl_dock_test_4_15_24 .csv')
 
 # Clean the 'NIR (Raw)' column
 df['NIR (Raw)'] = pd.to_numeric(df['NIR (Raw)'], errors='coerce')
@@ -10,11 +11,8 @@ df['NIR (Raw)'] = pd.to_numeric(df['NIR (Raw)'], errors='coerce')
 # Remove rows with NaN values in the 'NIR (Raw)' column
 df_csv = df.dropna(subset=['NIR (Raw)'])
 
-# Filter the DataFrame to get rows where 'Measurement Type' is 'turbidity'
-turbidity_data = df_csv[df_csv['Measurement Type'] == 'turbidity']
-
 # Extract 'NIR (Raw)' and 'Milliseconds' values
-nir_values = turbidity_data['NIR (Raw)']
+NIR_values = turbidity_data['NIR (Raw)']
 milliseconds_values = turbidity_data['Milliseconds']
 
 # Solve for x using the equation y = 79.5485x + 1507.2419
@@ -27,3 +25,4 @@ plt.ylabel('FNU')
 plt.title('Turbidity Measurements vs. Milliseconds')
 plt.grid(True)
 plt.show()
+ 
